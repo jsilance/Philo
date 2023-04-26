025bdc5f3c52b849160a37c9cbee8d3c9951ff93
Julien Silance <jusilanc@shi-r12-p1.s19.be>
2023-04-26 15:59:15 +0200

    maj

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 14:12:48 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/04/26 14:27:52 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/04/26 15:58:24 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
+	int	i;
+	int	id;
+
 	(void)argv;
+	i = argc - 1;
+	while (i >= 0)
+	{
+		id = fork();
+		printf("");
+	}
 	return (0);
 }
