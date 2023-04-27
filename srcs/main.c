b50d60c45a7489671d6adc99f454abce9716bca9
Julien Silance <jusilanc@shi-r5-p1.s19.be>
2023-04-27 19:48:23 +0200

    push

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 14:12:48 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/04/26 15:58:24 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/04/27 14:28:18 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	id;
+	pthread_t	ptr;
+	pthread_t	ptr2;
 
 	(void)argv;
-	i = argc - 1;
-	while (i >= 0)
-	{
-		id = fork();
-		printf("");
-	}
+	(void)argc;
+	if (pthread_create(&ptr, NULL, (void *)printf, (void *)"yolo\n"))
+		return (1);
+	if (pthread_create(&ptr2, NULL, (void *)printf, (void *)"yolo1\n"))
+		return (1);
+	pthread_join(ptr, NULL);
+	pthread_join(ptr2, NULL);
 	return (0);
 }
