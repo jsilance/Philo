b64a3600efb7239402207569848b4f59eb59f21c
jsilance <juliensilance@gmail.com>
2023-04-28 04:27:49 +0200

    arg to struct with checker done

--- Makefile
+++ Makefile
@@ -3,25 +3,27 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+         #
+#    By: jusilanc <jusilanc@student.s19.be>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/11 21:01:16 by jusilanc          #+#    #+#              #
-#    Updated: 2023/04/26 14:29:42 by jusilanc         ###   ########.fr        #
+#    Updated: 2023/04/28 03:43:51 by jusilanc         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = philo
 
-SRCS = main.c \
+SRCS = main.c ft_philo_init.c ft_atoi.c ft_isdigit.c \
 
 SRC = $(addprefix srcs/, $(SRCS))
 
 CFLAGS = -Wall -Wextra -Werror -I includes/
 
+LDFLAGS = -pthread
+
 OBJ = ${SRC:.c=.o}
 
 ${NAME}: ${OBJ}
-		@cc ${CFLAGS} ${OBJ} -o ${NAME}
+		@cc ${CFLAGS} ${OBJ} ${LDFLAGS} -o ${NAME}
 
 all: ${NAME}
 
--- includes/ft_philo.h
+++ includes/ft_philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_philo.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jusilanc <jusilanc@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 14:13:23 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/04/26 14:24:10 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/04/28 04:03:48 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,19 @@
 # include <string.h>
 # include <sys/time.h>
 # include <unistd.h>
+
+typedef struct s_philo
+{
+	int	nb_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	nb_to_eat;
+}		t_philo;
+
+int		ft_philo_init(t_philo *philo, char **argv, int argc);
+int		ft_atoi(const char *str);
+int		ft_str_isdigit(const char *str);
+int		ft_isdigit(int c);
 
 #endif
